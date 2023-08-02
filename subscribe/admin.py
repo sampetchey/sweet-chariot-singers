c971a8ac2dab6fb24d6abc917ccce6335b762379
sampetchey <sam_petchey@yahoo.com>
2023-08-02 21:57:26 +0100

    add form admin placeholders and fields

--- subscribe/admin.py
+++ subscribe/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Membership
 
-# Register your models here.
+class MembershipAdmin(admin.ModelAdmin):
+    readonly_fields = ('membership_fee', 'date',)
+    fields = ('date', 'full_name', 'email', 'phone_number', 
+              'postcode', 'town_or_city', 'street_address1', 
+              'street_address2', 'voice_type', 'membership_fee',)
+
+admin.site.register(Membership, MembershipAdmin)
