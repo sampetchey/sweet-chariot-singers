8e0a5311ab3ecf95d4b713841293c9cda8f19b3d
sampetchey <sam_petchey@yahoo.com>
2023-08-09 17:23:39 +0100

    configure subscribe/views.py, adding checkout session. js not working onclick

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.subscribe, name="subscribe"),
+    path('', views.HomePageView.as_view(), name='home'),
+    path('config/', views.stripe_config),
+    path('create-checkout-session/', views.create_checkout_session),
 ]
  
